0d409a6416e6d6da483a96f8dc366c01c31acd8b
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-06-05 15:38:46 +0500

    refactor: remove unused pre_delete and post_save signals for Topic model

--- apps/pedagog/signals/signals.py
+++ apps/pedagog/signals/signals.py
@@ -1,33 +1,32 @@
-from django.db.models.signals import post_save, pre_delete, m2m_changed
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 
 from apps.pedagog.models.moderator import Moderator
-from apps.pedagog.models.topic import Topic
 from apps.users.choices.role import Role
 from apps.users.models.user import User, ContractStatus
 from apps.websocket.models.notification import Notification
 
 
-@receiver(pre_delete, sender=Topic)
-def reorder_topics_on_delete(sender, instance, **kwargs):
-    related_topics = list(
-        Topic.objects.filter(plan_id=instance.plan_id)
-        .exclude(id=instance.id)
-        .order_by("sequence_number")
-    )
-    for index, topic in enumerate(related_topics, start=1):
-        topic.sequence_number = index
-    Topic.objects.bulk_update(related_topics, ["sequence_number"])
-
-
-@receiver(post_save, sender=Topic)
-def reorder_topics_on_save(sender, instance, created, **kwargs):
-    related_topics = list(
-        Topic.objects.filter(plan_id=instance.plan_id).order_by("sequence_number")
-    )
-    for index, topic in enumerate(related_topics, start=1):
-        topic.sequence_number = index
-    Topic.objects.bulk_update(related_topics, ["sequence_number"])
+# @receiver(pre_delete, sender=Topic)
+# def reorder_topics_on_delete(sender, instance, **kwargs):
+#     related_topics = list(
+#         Topic.objects.filter(plan_id=instance.plan_id)
+#         .exclude(id=instance.id)
+#         .order_by("sequence_number")
+#     )
+#     for index, topic in enumerate(related_topics, start=1):
+#         topic.sequence_number = index
+#     Topic.objects.bulk_update(related_topics, ["sequence_number"])
+#
+#
+# @receiver(post_save, sender=Topic)
+# def reorder_topics_on_save(sender, instance, created, **kwargs):
+#     related_topics = list(
+#         Topic.objects.filter(plan_id=instance.plan_id).order_by("sequence_number")
+#     )
+#     for index, topic in enumerate(related_topics, start=1):
+#         topic.sequence_number = index
+#     Topic.objects.bulk_update(related_topics, ["sequence_number"])
 
 
 @receiver(m2m_changed, sender=User.document.through)  # M2M relationship changes
