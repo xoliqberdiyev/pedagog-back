756bfa8cb5bf67f9e1257c674bddc90b03d685f8
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-06-17 12:30:17 +0500

    feat: enhance ModeratorPermissionSerializer to include related school and class details

--- apps/moderator/serializers/permission.py
+++ apps/moderator/serializers/permission.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
 from apps.moderator.models.permission import ModeratorPermission
+from apps.pedagog.serializers.classes import ClassesSerializer
+from apps.pedagog.serializers.school import SchoolTypeSerializer
+from apps.pedagog.serializers.science import ScienceSerializer
 
 
 class ModeratorPermissionSerializer(serializers.ModelSerializer):
@@ -25,3 +28,14 @@ class ModeratorPermissionSerializer(serializers.ModelSerializer):
             "user": {"read_only": True},
             "status": {"read_only": True},
         }
+
+    def to_representation(self, instance):
+        """
+        Custom representation to include user details.
+        """
+        representation = super().to_representation(instance)
+        representation["school_type"] = SchoolTypeSerializer(instance.school_type, many=True).data
+        representation["classes"] = ClassesSerializer(instance.classes, many=True).data
+        representation["science"] = ScienceSerializer(instance.science, many=True).data
+        representation["science_language"] = ScienceSerializer(instance.science_language, many=True).data
+        return representation
