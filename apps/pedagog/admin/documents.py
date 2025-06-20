9f7ebb463574d65a1b5e0437f4bea63ef64e741d
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-06-20 17:36:44 +0500

    fix

--- apps/pedagog/admin/documents.py
+++ apps/pedagog/admin/documents.py
@@ -6,7 +6,7 @@ from apps.pedagog.models.documents import Document
 
 @admin.register(Document)
 class DocumentAdmin(ModelAdmin):
-    list_display = ("title", "description", "file", "type", "size")
+    list_display = ("id", "title", "description", "file", "type", "size")
     search_fields = ("title", "description", "file", "type")
     list_filter = ("is_active", "type")
     readonly_fields = ("size", "type")
