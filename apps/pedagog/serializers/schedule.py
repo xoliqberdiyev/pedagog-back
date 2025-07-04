19ed14050b7c2d4015a1e0dd0d219e0a5091e5c7
A'zamov Samandar <admin@jscorp.uz>
2025-07-04 17:45:53 +0500

    update serializer

--- apps/pedagog/serializers/schedule.py
+++ apps/pedagog/serializers/schedule.py
@@ -3,15 +3,24 @@ from rest_framework import serializers
 from apps.pedagog.models.schedule import LessonSchedule
 from apps.pedagog.serializers.quarter import QuarterMiniSerializer
 from apps.pedagog.serializers.classes import ClassesSerializer, ClassGroupSerializer
-from apps.pedagog.serializers.science import ScienceSerializer, ScienceLanguageSerializer
+from apps.pedagog.serializers.science import (
+    ScienceSerializer,
+    ScienceLanguageSerializer,
+)
 
 
 class LessonScheduleSerializer(serializers.ModelSerializer):
-    quarter = QuarterMiniSerializer()
-    classes = ClassesSerializer()
-    class_group = ClassGroupSerializer()
-    science = ScienceSerializer()
-    science_language = ScienceLanguageSerializer()
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["quarter"] = QuarterMiniSerializer(instance.quarter).data
+        data["classes"] = ClassesSerializer(instance.classes).data
+        data["class_group"] = ClassGroupSerializer(instance.class_group).data
+        data["science"] = ScienceSerializer(instance.science).data
+        data["science_language"] = ScienceLanguageSerializer(
+            instance.science_language
+        ).data
+        return data
 
     class Meta:
         model = LessonSchedule
